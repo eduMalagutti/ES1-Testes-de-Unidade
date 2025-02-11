f4b0faae3984c27cc38e2543688f54050ece6c11
Eduardo Malagutti <eduardo.malagutti@catijr.com.br>
2025-02-11 13:36:11 -0300

    Init spring boot project

---
+++ src/main/java/com/malagutti/es1_unit_tests/Es1UnitTestsApplication.java
@@ -0,0 +1,13 @@
+package com.malagutti.es1_unit_tests;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Es1UnitTestsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Es1UnitTestsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/malagutti/es1_unit_tests/Es1UnitTestsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.malagutti.es1_unit_tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Es1UnitTestsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
